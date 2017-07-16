5e856b1f4c880d71e737d807bf6e758808cef0d5
Nicole Martin <nikismar@hotmail.com>
2017-07-16 18:56:01 -0500

    pics/descriptions showing on DOM

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -1,1 +1,42 @@
 console.log('JS sourced');
+
+var myApp = angular.module('myApp', []);
+
+//gallery controller function beginning
+myApp.controller('GalleryController', function() {
+  var gallery = this;
+  console.log(this);
+
+  //gallery.pictures = [];
+  //gallery.addPicture = function(src, description, likes, likeButton) {
+    gallery.picture = [
+      {
+        src: '/images/Cinderella-castle.jpg',
+        description: "Cinderella's Castle",
+        likes: 0,
+        likeButton: false,
+        display: true
+      },
+      {
+        src: '/images/epcot.jpg',
+        description: "Epcot at night",
+        likes: 0,
+        likeButton: false,
+        display: true
+      },
+      {
+        src: '/images/toystoreride.jpg',
+        description: "Toystory ride",
+        likes: 0,
+        likeButton: false,
+        display: true
+      }
+    ];
+
+      console.log(gallery.picture);
+
+// on click of picture, the description will show.
+    gallery.changeDisplay = function(picture) {
+      picture.display = !picture.display;
+    };
+}); //gallery controller function end
